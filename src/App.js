3456550db66204294bc07bfe59395f8b4c72ff8f
yun <2212032@exchange.prince.com.tw>
2023-03-13 14:57:26 +0800

    Add Bootstrap style

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
           Your Todolist
         </div>
-        <div>
-          <table>
+        <div className='card-body'>
+          <table className='table table-hover'>
             <thead>
-             <tr>
-               <th scope='col'>#</th>
-               <th scope='col'>Description</th>
-               <th scope='col'>Assigned</th>
-             </tr>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Assigned</th>
+            </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>task1</td>
-                <td>Eric</td>
-              </tr>
-              <tr>
-                <th scope='row'>2</th>
-                <td>task2</td>
-                <td>Eric</td>
-              </tr>
+            <tr>
+              <th scope='row'>1</th>
+              <td>task1</td>
+              <td>Eric</td>
+            </tr>
+            <tr>
+              <th scope='row'>2</th>
+              <td>task2</td>
+              <td>Eric</td>
+            </tr>
             </tbody>
           </table>
         </div>
